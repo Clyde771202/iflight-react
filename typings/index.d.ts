4696a8c09ce12538331cceca071ed39f81bfc347
陈路遥 <cly14183@ly.com>
2018-01-09 17:49:58 +0800

    11

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -3,3 +3,5 @@
 /// <reference path="globals/fastclick/index.d.ts" />
 /// <reference path="globals/immutable/index.d.ts" />
 /// <reference path="globals/iscroll/index.d.ts" />
+/// <reference path="globals/react-router/index.d.ts" />
+/// <reference path="globals/webpack-dev-server/index.d.ts" />
