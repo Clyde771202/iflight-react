30342dbc908e152182c2fd36cb277643552f25a9
陈路遥 <cly14183@ly.com>
2018-01-09 15:22:48 +0800

    11

---
+++ typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="globals/axios/index.d.ts" />
+/// <reference path="globals/es6-promise/index.d.ts" />
+/// <reference path="globals/fastclick/index.d.ts" />
+/// <reference path="globals/immutable/index.d.ts" />
+/// <reference path="globals/iscroll/index.d.ts" />
