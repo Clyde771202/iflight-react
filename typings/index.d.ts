90755d2264fa5de9d95cb56de8aac301f909440a
陈路遥 <cly14183@ly.com>
2018-01-09 17:50:53 +0800

    update .gitignore

--- typings/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="globals/axios/index.d.ts" />
-/// <reference path="globals/es6-promise/index.d.ts" />
-/// <reference path="globals/fastclick/index.d.ts" />
-/// <reference path="globals/immutable/index.d.ts" />
-/// <reference path="globals/iscroll/index.d.ts" />
-/// <reference path="globals/react-router/index.d.ts" />
-/// <reference path="globals/webpack-dev-server/index.d.ts" />
